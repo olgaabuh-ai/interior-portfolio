f00549c268485aae0c94e2e67185b7f5ac1dc453
olgaabuh-ai <olgaabuh@gmail.com>
2026-05-01 19:26:08 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-2import react from '@vitejs/plugin-react'
-3
-4export default defineConfig({
-5  plugins: [react()],
-6})
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
