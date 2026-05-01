bd04a46482fae95a64d74cdfd88598bc26dc3d37
olgaabuh-ai <olgaabuh@gmail.com>
2026-05-01 15:28:18 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+2import react from '@vitejs/plugin-react'
+3
+4export default defineConfig({
+5  plugins: [react()],
+6})
