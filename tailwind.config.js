21c83399d3ba0b0fe9b73a9d0084bd1140104cf9
olgaabuh-ai <olgaabuh@gmail.com>
2026-05-01 19:27:07 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'vertical-scroll': 'vertical-scroll 30s linear infinite',
+      },
+      keyframes: {
+        'vertical-scroll': {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-50%)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
