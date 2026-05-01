f44cae3493e2fd8a6c1cd0a1ea7121c8d2c14088
olgaabuh-ai <olgaabuh@gmail.com>
2026-05-01 19:33:49 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-2import InteriorPortfolio from "./App";
-3import "./index.css";
-4
-5export default function Component() {
-6    return <InteriorPortfolio />;
-7}
+import ReactDOM from "react-dom/client";
+import InteriorPortfolio from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <InteriorPortfolio />
+  </React.StrictMode>
+);
